848fe40ddbbea4ee2c82cfee1ceac4b5789f16f0
Dom <157792207+lunarjuun@users.noreply.github.com>
2025-08-26 14:38:59 +0100

    små oppdateringer

--- script.js
+++ script.js
@@ -1,32 +1,92 @@
-const API_KEY = "DEMO_KEY";
-const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 
-const imageEl = document.getElementById.apply("apodImage");
-const titleEl = document.getElementById("title");
-const explanationEl = document.getElementById("explanation");
 
-async function fetchAPOD() {
-    try {
-        const res = await fetch(URL);
-        const data = res.json();
 
-        titleEl.textContent = data.title;
-        explanationEl.textContent = data.explanation;
 
-        if (data.media_type === "image") {
-            imageEl.src = data.url;
-            imageEl.Elalt = data.title;
-        } else {
-            imageEl.remove();
-            titleEl.insertAdjacentHTML(
-                "afterend",
-                <p>(Dagens bilde er dette: <a href="${data.url}" target="_blank">Se her</a>)</p>
-            )
-        }
-    } catch (err) {
-        title.El.textContent = "Kunne ikke hente bilde.";
-        console.error(err);
-    }
-}
 
-fetchAPOD();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const API_KEY = "DEMO_KEY";
+// const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+// const imageEl = document.getElementById("apodImage");
+// const titleEl = document.getElementById("title");
+// const explanationEl = document.getElementById("explanation");
+
+// async function fetchAPOD() {
+//   try {
+//     const res = await fetch(url);
+//     const data = await res.json();
+
+//     if (data.media_type === "image") {
+//       // 👉 bilde
+//       titleEl.textContent = data.title;
+//       explanationEl.textContent = data.explanation;
+//       imageEl.src = data.url;
+//       imageEl.alt = data.title;
+//       imageEl.hidden = false;
+//     } else {
+//       // 👉 video
+//       titleEl.textContent = "Ingen bilde i dag 😢";
+//       explanationEl.innerHTML = `Dagens APOD er en video: <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">Se her</a>`;
+//       imageEl.hidden = true;
+//     } 
+//   } catch (err) {
+//     titleEl.textContent = "Kunne ikke hente bilde 😢";
+//     console.error(err);
+//   }
+// }
+
+// fetchAPOD();
