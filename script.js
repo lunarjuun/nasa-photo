ad130ff6bf075bb24b7d0304b56023d93bf7e609
Dom <157792207+lunarjuun@users.noreply.github.com>
2025-08-27 09:14:18 +0100

    adding font changer

--- script.js
+++ script.js
@@ -9,76 +9,12 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
 
 document.addEventListener('DOMContentLoaded', function() {
     const fontChangerBtn = document.getElementById('fontChangerBtn');
-    const contentToChange = document.getElementById('contentToChange');
+    const body = document.body;
 
     fontChangerBtn.addEventListener('click', function() {
-        contentToChange.classList.toggle('new-font');
+        body.classList.toggle('sans');
+        fontChangerBtn.textContent = body.classList.contains('sans') 
+            ? "Mansalva version" 
+            : "Standard text version";
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const API_KEY = "DEMO_KEY";
-// const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
-
-// const imageEl = document.getElementById("apodImage");
-// const titleEl = document.getElementById("title");
-// const explanationEl = document.getElementById("explanation");
-
-// async function fetchAPOD() {
-//   try {
-//     const res = await fetch(url);
-//     const data = await res.json();
-
-//     if (data.media_type === "image") {
-//       // 👉 bilde
-//       titleEl.textContent = data.title;
-//       explanationEl.textContent = data.explanation;
-//       imageEl.src = data.url;
-//       imageEl.alt = data.title;
-//       imageEl.hidden = false;
-//     } else {
-//       // 👉 video
-//       titleEl.textContent = "Ingen bilde i dag 😢";
-//       explanationEl.innerHTML = `Dagens APOD er en video: <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">Se her</a>`;
-//       imageEl.hidden = true;
-//     } 
-//   } catch (err) {
-//     titleEl.textContent = "Kunne ikke hente bilde 😢";
-//     console.error(err);
-//   }
-// }
-
-// fetchAPOD();
